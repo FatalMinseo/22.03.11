7a4bca4339fb3373c542dbe156f9f6e819ad5c2c
장민서 <95062577+FatalMinseo@users.noreply.github.com>
2022-03-11 23:58:54 +0900

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,11 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		// 사람의 이름과 나이
+		MemberVO vo = new MemberVO("장민서", 20);
+//		vo.setName("장민서");
+//		vo.setAge(20);
+		System.out.println(vo.toString());
+	}
+}
